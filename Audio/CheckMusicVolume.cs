e4f77301457aa9b3579c40caccccd5f64bcfa4a3
radicalmethodz <95551620+radicalmethodz@users.noreply.github.com>
2023-06-30 19:55:22 +0200

    Add files via upload

---
+++ Audio/CheckMusicVolume.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SlimUI.ModernMenu{
+	public class CheckMusicVolume : MonoBehaviour {
+		public void  Start (){
+			// remember volume level from last time
+			GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
+		
+			// Update the volume immediately
+			UpdateVolume();
+		}
+
+		public void UpdateVolume (){
+			GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
+		}
+	}
+}
